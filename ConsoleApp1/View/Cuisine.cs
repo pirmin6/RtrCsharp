baf33288d169d9d58fa5c5780651c3b621732673
BenjaminExia <benjamin.rebattu@viacesi.fr>
2019-05-21 23:32:11 +0200

    Ajout vciew

---
+++ ConsoleApp1/View/Cuisine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1.View
+{
+    public partial class Cuisine : Form
+    {
+        public Cuisine()
+        {
+            InitializeComponent();
+        }
+    }
+}
