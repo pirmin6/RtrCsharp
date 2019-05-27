211d7247638f88079cb405256bf1c69c8a16fb42
BenjaminExia <benjamin.rebattu@viacesi.fr>
2019-05-27 17:23:45 +0200

    commencer damier + vue image

--- ConsoleApp1/View/Cuisine.Designer.cs
+++ ConsoleApp1/View/Cuisine.Designer.cs
@@ -30,8 +30,10 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.ClientSize = new System.Drawing.Size(1000, 600);
             this.Text = "Cuisine";
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
         }
 
         #endregion
--- ConsoleApp1/View/Cuisine.cs
+++ ConsoleApp1/View/Cuisine.cs
@@ -17,16 +17,25 @@ namespace ConsoleApp1.View
         {
             InitializeComponent();
 
-            FromImageImage(PaintEventArgs e);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.imageDessin);
 
-            void FromImageImage(PaintEventArgs e)
-            {
-                Image imageFile = Image.FromFile("smiley.jpg");
-                Graphics commis = Graphics.FromImage(imageFile);
+        }
 
-                commis.FillRectangle(new SolidBrush(Color.Black), 100, 50, 100, 100);
-                e.Graphics.DrawImage(imageFile, new PointF(0.0F, 0.0F));
-            }
+        public void damier(object sender, PaintEventArgs e)
+        {
+            Pen blackPen = new Pen(Color.Black, 3);
+            List<Rectangle> ListCarre = new List<Rectangle>();
+
+           // for 
+
+        }
+
+        private void imageDessin(object sender, PaintEventArgs e)
+        {
+            Image image = Image.FromFile("D:/POO/CSHARP/RtrCsharp/asset/smiley.jpg");
+            // Par exemple ici on mettrait en argument image, maitreHotel.x, maitreHotel.y, maitrehotel.width, maitreHotel.height
+            e.Graphics.DrawImage(image, 200, 250, 50,50);
+            //e.Graphics.DrawRectangle()
         }
 
     }
